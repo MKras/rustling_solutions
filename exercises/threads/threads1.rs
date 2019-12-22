19978000843b138ef7dc35cd1a4c8feada613712
Mikhail Krasikau <m.krasikau@gmail.com>
2019-12-22 09:46:15 +0300

    threads are done!

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -5,9 +5,7 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -16,16 +14,33 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let status_shared = status.clone();
+        
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
+            let mut status_shared = status_shared.lock().unwrap();
             status_shared.jobs_completed += 1;
-        }
+            println!("Thread: {}", status_shared.jobs_completed);
+        };
     });
-    while status.jobs_completed < 10 {
+    
+    // more verbose
+    let mut jobs_completed = 0;    
+    while jobs_completed < 10  {
+        {
+            let status_guard = status.lock().unwrap();
+            jobs_completed = status_guard.jobs_completed;
+            println!("jobs_completed {}", jobs_completed);
+        }
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
+
+    // // or just like here 
+    // while status.lock().unwrap().jobs_completed < 10 {
+    //     println!("waiting... ");
+    //     thread::sleep(Duration::from_millis(500));
+    // }    
 }
